9470a1d38cd24e9ac91918733a169400d0984aed
umidjan143 <xasanbayevumidjon@gmail.com>
2023-08-01 11:17:25 -0400

    homework

---
+++ main.js
@@ -0,0 +1,156 @@
+
+
+//1
+
+// const num = [ 33, 12, 55, 44, 8, 77, 66, 51, 84, 72, 64, 32, 87];
+
+
+
+ 
+// const newm =  num.map((item) => item*2)
+// const newf =  num.filter((item) => item>= 40)
+// const news = num.sort((a,b) => a-b)
+// const newr = num.reduce((pre,cur) => cur+pre ) ; 
+
+
+
+//2
+
+
+// const num2 = [ 15, 44 , 84, 54, 78, 62 , 12, 33, 10, 5, 99, 77, 53];
+
+
+
+ 
+// const newm =  num2.map((item) => item*3)
+// const newf =  num2.filter((item) => item>= 30)
+// const news = num2.sort((a,b) => a-b)
+// const newr = num2.reduce((pre,cur) => cur+pre ) ; 
+
+
+
+
+
+
+//3
+
+// const num3 = [ 20, 32, 12, 10, 44, 71, 17, 24, 66, 41, 33];
+
+
+
+ 
+// const newm =  num3.map((item) => item*4)
+// const newf =  num3.filter((item) => item>= 35)
+// const news = num3.sort((a,b) => a-b)
+// const newr = num3.reduce((pre,cur) => cur+pre ) ; 
+
+
+
+
+
+
+//4
+
+
+// const num4 = [ 20, 32, 12, 10, 44, 71, 17, 24, 66, 41, 33];
+
+
+
+ 
+// const newm =  num4.map((item) => item*5)
+// const newf =  num4.filter((item) => item>= 50)
+// const news = num4.sort((a,b) => a-b)
+// const newr = num4.reduce((pre,cur) => cur+pre ) ; 
+
+
+
+//5
+
+
+
+// const num5 = [ 2, 6, 4, 88, 12 , 31, 74, 51,23, 17];
+
+
+
+ 
+// const newm =  num5.map((item) => item*5)
+// const newf =  num5.filter((item) => item<= 50)
+// const news = num5.sort((a,b) => a-b)
+// const newr = num5.reduce((pre,cur) => cur+pre ) ; 
+
+
+// const newfe = num5.forEach((ages) => console.log(ages))
+
+
+
+
+
+//6
+
+
+// const num6 = [ 21, 12, 41, 14, 32, 23, 51, 15, 64, 46];
+
+
+
+ 
+// const newm =  num6.map((item) => item*1.5)
+// const newf =  num6.filter((item) => item<= 50)
+// const news = num6.sort((a,b) => a-b)
+// const newr = num6.reduce((pre,cur) => cur+pre ) ; 
+
+
+// const newfe = num6.forEach((ages) => console.log(ages))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const num = [ 21, 12, 41, 14, 32, 23, 51, 15, 64, 46];
+
+// const combined = num 
+// .map((item) => item*2)
+// .filter((item) => item>= 40)
+// .sort((a,b) => a-b)
+// .reduce((pre,cur) => cur+pre ) ; 
+
+
+
+
+
+// const num2 = [ 20, 32, 12, 10, 44, 71, 17, 24, 66, 41, 33];
+
+// const combined2 = num2 
+// .map((item) => item*2)
+// .filter((item) => item>= 40)
+// .sort((a,b) => a-b)
+// .reduce((pre,cur) => cur+pre ) ; 
+
+
+
+
+
+
+const num3 = [ 20, 32, 12, 10, 44, 71, 17, 24, 66, 41, 33];
+
+const combined2 = num2 
+.map((item) => item*2)
+.filter((item) => item>= 40)
+.sort((a,b) => a-b)
+.reduce((pre,cur) => cur+pre ) ; 
+
+
+
+
+
